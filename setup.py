fc0fbed237b0c390f5fa2e1d389bc7df57c61f68
Ian <ianquah@hotmail.com>
2016-10-16 12:28:27 -0400

    Release 1.0.2 successful

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     packages=["DatasetZoo", "DatasetZoo/utils",
               "DatasetZoo/downloaded_datasets",
               "DatasetZoo_backend"],
-    version='1.0.1',
+    version='1.0.2',
     description='Easily share datasets within your institution\
     or with the rest of the world!',
     # The project's main homepage.
     url='https://github.com/IanQS/DatasetZoo',
-    download_url='https://github.com/IanQS/DatasetZoo/releases/tag/1.0.1',
+    download_url='https://github.com/IanQS/DatasetZoo/releases/tag/1.0.2',
 
     # Author details
     author='Ian Quah',
