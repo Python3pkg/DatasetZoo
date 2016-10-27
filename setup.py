6b3d9d32091d5c9ddbacd6d635fa215d79e10b55
Ian <ianquah@hotmail.com>
2016-10-27 00:32:08 -0400

    woo 1.1.0

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     name='DatasetZoo',
     packages=["utils", "downloaded_datasets"],
 #    ,"DatasetZoo_backend"],
-    version='1.0.4',
+    version='1.1.0',
     description='Easily share datasets within your institution\
     or with the rest of the world!',
     # The project's main homepage.
     url='https://github.com/IanQS/DatasetZoo',
-    download_url='https://github.com/IanQS/DatasetZoo/releases/tag/1.0.4',
+    download_url='https://github.com/IanQS/DatasetZoo/releases/tag/1.1.0',
 
     # Author details
     author='Ian Quah',
